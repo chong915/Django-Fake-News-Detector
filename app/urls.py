a8493e099a8b58a614fb60468cdfb6e7e9834d63
munchong <munchong915@hotmail.com>
2021-12-24 16:00:18 +0800

    Modified absolute path

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
 	path('', views.home, name="app-home"),
 	path('about', views.about, name='app-about'),
-	path('article//', views.article, name="app-article")
+	path('article', views.article, name="app-article")
 ]
